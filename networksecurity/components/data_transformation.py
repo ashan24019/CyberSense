99d18c7eb966bb902143f4e3b6969fb68a9abc83
Ashan <ashanvimodh241@gmail.com>
2026-03-24 09:47:26 +0530

    model pusher

--- networksecurity/components/data_transformation.py
+++ networksecurity/components/data_transformation.py
@@ -68,6 +68,8 @@ class DataTransformation:
         save_numpy_array_data( self.data_transformation_config.transformed_test_file_path, array=test_arr,)
         save_object( self.data_transformation_config.transformed_object_file_path, preprocessor_obj,)
 
+        save_object("final_models/model.pkl", preprocessor_obj)
+
         #Preparing Artifact
 
         data_transformation_artifact = DataTransformationArtifact(
--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -100,6 +100,8 @@ class ModelTrainer:
         Network_Model = NetworkModel(preprocessor=preprocessor, model=best_model)
         save_object(self.model_trainer_config.trained_model_file_path, obj=Network_Model)
 
+        save_object("final_models/model.pkl", best_model)
+
         #Model trainer Artifact
         model_trainer_artifact = ModelTrainerArtifact(trained_model_file_path= self.model_trainer_config.trained_model_file_path,
                              train_metric_artifact=classification_train_metrics,
