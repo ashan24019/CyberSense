8d1945e91e780ab16be921b27cb7bd0b1ada24b1
Ashan <ashanvimodh241@gmail.com>
2026-03-24 17:57:50 +0530

    training pypeline and fast api app

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -7,8 +7,6 @@ from networksecurity.utils.ml_utils.model.estimator import NetworkModel
 import sys
 
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import r2_score
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import (
     AdaBoostClassifier,
