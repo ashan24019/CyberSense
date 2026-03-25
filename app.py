d67148ae3153ae8809d3dbd0a0f209940057ed54
Ashan <ashanvimodh241@gmail.com>
2026-03-25 06:48:32 +0530

    predict API added

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ from fastapi.responses import Response
 from starlette.responses import RedirectResponse
 import pandas as pd
 from networksecurity.utils.main_utils.utils import load_object
+from networksecurity.utils.ml_utils.model.estimator import NetworkModel
 
 client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
 
@@ -42,6 +43,9 @@ app.add_middleware(
     allow_headers = ["*"]
 )
 
+from fastapi.templating import Jinja2Templates
+templates = Jinja2Templates(directory = "./templates")
+
 @app.get("/", tags=["authentication"])
 async def index():
     return RedirectResponse(url="/docs")
@@ -52,6 +56,24 @@ async def train_route():
         train_pipeline = TrainingPipeline()
         train_pipeline.run_pipeline()
         return Response("Training is Successfull")
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+
+@app.get("/predict")
+async def predict_route(request:Request, file: UploadFile = File(...)):
+    try:
+
+        df = pd.read_csv(file.file)
+        preprocessor = load_object("final_model/preprocessor.pkl")
+        final_model = load_object("final_model/model.pkl")
+        network_model = NetworkModel(preprocessor=preprocessor, model=final_model)
+        y_pred = network_model.predict(df)
+        df['predicted_column'] = y_pred
+        df.to_csv("prediction_output/output.csv")
+        table_html = df.to_html(classes='table table-striped')
+
+        return templates.TemplateResponse("table.html", {"request": request, "table": table_html})
+
     except Exception as e:
         raise NetworkSecurityException(e,sys)
     
